cc7ce24adfce7c0ed43043ba05c6476d3134f9b5
Kijima Daigo <norimaking777@gmail.com>
2024-09-11 21:15:00 +0900

    feat: ビデオモード
    day03/harib00h

--- haribote.s
+++ haribote.s
@@ -1,12 +1,31 @@
-.text
-.code16
+.text   # 実行可能なコードなことを示す
+.code16 # CPUは16ビットリアルモードで動作し、コードは16ビット命令セットで書かれている
 
-# ビデオモード
+# シンボルを定義する
+.set CYLS, 0x0ff0
+.set LEDS, 0x0ff1
+.set VMODE, 0x0ff2 # n bit color
+.set SCRNX, 0x0ff4 # display resolution X
+.set SCRNY, 0x0ff6 # display resolution Y
+.set VRAM, 0x0ff8  # VRAMの先頭メモリ。VRAMは解像度によってメモリマップの中の複数の場所にあるためメモする
+
+# ビデオモード vga graphics 320x200 32bit color
 movb $0x13, %al
 # ビデオモード設定
 movb $0x00, %ah
 # BIOS割り込みでビデオモードを変更する
 int $0x10
+
+# save screen information
+movb $8, (VMODE)
+movw $320, (SCRNX)
+movw $200, (SCRNY)
+movl $0x000a0000, (VRAM)
+
+# get keyboard led status from BIOS
+mov $0x02, %ah
+int $0x16
+mov %al, (LEDS)
 
 fin:
         hlt
