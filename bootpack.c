03e3f68378bac8738604ae232e56c5a8b7b45b84
Kijima Daigo <norimaking777@gmail.com>
2024-09-14 10:41:28 +0900

    feat: 矩形を表示する

--- bootpack.c
+++ bootpack.c
@@ -1,3 +1,21 @@
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -5,6 +23,7 @@ int io_load_eflags(void);
 void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
 void HariMain(void){
   int i;
@@ -14,17 +33,22 @@ void HariMain(void){
 
   p = (char *)0xa0000;
 
-  for(i = 0xa0000; i <= 0xaffff; i++) {
-    /* 番地を代入する */
-    p = (char *) i;
-    /* 番地の値を代入する */
-    /* 縞模様。下位4ビットを残す。16画素ごとに色番号が繰り返される */
-    *p = i & 0x0f;
-  }
+  boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+  boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+  boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
 
   for(;;) {
     io_hlt();
   }
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+    int x, y;
+    for(y = y0; y <= y1; y++){
+        for(x = x0; x <= x1; x++){
+            vram[y * xsize + x] = c;
+        }
+    }
 }
 
 void init_palette(void) {
